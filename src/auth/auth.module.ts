86deb6dcdcbd17fd41d8720dbebb3f20a670c1c0
KJH <kjhg5852@naver.com>
2021-03-23 09:15:41 +0900

    공식 문서 passport-jwt 구현

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+  ],
+  exports: [AuthService],
 })
 export class AuthModule {}
